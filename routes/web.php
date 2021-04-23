bb5dd365a2d0552b2a494e99bfa934fcb9a299c4
hzx name <hzxemail@gmail.com>
2021-04-23 21:10:37 +0800

    yemian commit

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+use App\Http\Controllers\StaticPagesController;
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [StaticPagesController::class, 'home']);
+Route::get('/help', [StaticPagesController::class, 'help']);
+Route::get('/about', [StaticPagesController::class, 'about']);
