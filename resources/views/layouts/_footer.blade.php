bab1cc83e706c70a2967d0e4af621373ecb758ba
hzx name <hzxemail@gmail.com>
2021-04-24 22:01:10 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,6 +2,6 @@
   <img class="brand-icon" src="/a.png" alt="">
   <a href="https://laravel-china.org/courses" target="_blank">刻意练习，每日精进</a>
   <div class="float-right">
-    <a href="/about">关于</a>
+    <a href="{{'about'}}">关于</a>
   </div>
 </footer>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use App\Http\Controllers\StaticPagesController;
     return view('welcome');
 });
 */
-Route::get('/', [StaticPagesController::class,'home']);
-Route::get('/helpp',[StaticPagesController::class,'helpp']);
-Route::get('/about', [StaticPagesController::class,'about']);
+Route::get('/', [StaticPagesController::class,'home'])->name('home');
+Route::get('/helpp',[StaticPagesController::class,'helpp'])->name('helpp');
+Route::get('/about', [StaticPagesController::class,'about'])->name('about');
